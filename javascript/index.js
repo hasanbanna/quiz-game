50947ff8f0ff261e5cf5a371035722ebbc2c8b10
Hasan Banna <hbann002@gold.ac.uk>
2017-03-11 18:39:03 +0000

    get data from quiz api

--- javascript/index.js
+++ javascript/index.js
@@ -1,2 +1,24 @@
 // fetch data from quiz API
 // https://opentdb.com/api.php?amount=10&type=multiple
+$(document).ready(function(){
+  var questions_answers={};
+  $.ajax({
+    url:"https://opentdb.com/api.php?amount=10&type=multiple",
+    type:'get',
+    dataType:'JSON',
+    success: function(result){
+      var l = result.results.length;
+      for(var i = 0; i < l; i++){
+        $("#quiz-area").append("<p>"+result.results[i].category +"</p>");
+        $("#quiz-area").append("<p>"+result.results[i].question +"</p>");
+        for(var j = 0; j < 3; j++){
+        $("#quiz-area").append("<p>"+result.results[i].incorrect_answers[j] +"</p>");
+        }
+        $("#quiz-area").append("<p>"+result.results[i].correct_answer+"</p>");
+      }
+
+    //  console.log(result);
+    }
+  });
+
+});
